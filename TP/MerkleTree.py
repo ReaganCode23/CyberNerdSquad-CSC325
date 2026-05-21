d30ecb8757d7ded107262e77e8bf84efa97c919f
GinnyB33 <vrb012@email.latech.edu>
2026-05-21 09:32:41 -0500

    separated the build-tree function into a first-hash function and a build-tree function, got the instert function working

--- TP/MerkleTree.py
+++ TP/MerkleTree.py
@@ -9,7 +9,8 @@ import hashlib
 class MerkleTree:
     def __init__(self, data_list):
         self.data_list = data_list
-        self.root = self._build_tree(self.data_list)
+        self.hashed_data = self._first_hash(self.data_list)
+        self.root = self._build_tree(self.hashed_data)
 
     def _hash(self, data):
         """
@@ -20,33 +21,65 @@ class MerkleTree:
         data = hashlib.sha256(data.encode('utf-8')).hexdigest()
         return data
 
-    def _build_tree(self, nodes):
+    def _first_hash(self, nodes):
 
-        #base case: return the root node once the tree is built
+        nodes_copy = []
+
+        for i in range(len(nodes)):
+            nodes_copy.append(nodes[i])
+
+        #debug
+        print(nodes)
+        print(nodes_copy)
+
         if len(nodes) == 1:
-            return nodes[0]
+            return self._hash(str(nodes[0]))
 
-        #this ensures that there is an even number of elements
         if len(nodes) % 2 != 0:
-            nodes.append(nodes[-1])
+            nodes_copy.append(nodes[-1])
 
-        #create a new list for the next level of the tree
+        #debug
+        print(nodes)
+        print(nodes_copy)
+
+        #create a new list for the hashed leaf nodes
+        hashed_data = []
+
+        for i in range(len(nodes_copy)):
+            hashed_data.append(self._hash(str(nodes_copy[i])))
+
+        #debug
+        print(hashed_data)
+
+        self.hashed_data = hashed_data
+        return hashed_data
+
+    def _build_tree(self, hashed_data):
+
+        if len(hashed_data) == 1:
+            self.root = hashed_data[0]
+            return hashed_data[0]
+
+        #create new list for the next level of the tree
         next_level = []
 
         #iterate through the leaves and hash every 2 nodes together
-        for i in range(0, len(nodes), 2):
-            hashed_data = self._hash(nodes[i] + nodes[i+1])
-            next_level.append(hashed_data)
+        for i in range(0, len(hashed_data), 2):
+            combined_hashed_data = self._hash(hashed_data[i] + hashed_data[i+1])
+            next_level.append(combined_hashed_data)
+
+        #debug
+        print(next_level)
 
         return self._build_tree(next_level)
 
     def print_tree(self):
-        print(self._build_tree(self.data_list))
+        print(self.root)
 
     def insert(self, new_data):
-        new_data = self._hash(new_data)
         self.data_list.append(new_data)
-        self._build_tree(self.data_list)
+        self._first_hash(self.data_list)
+        self.root = self._build_tree(self.hashed_data)
 
     '''def delete(self, data):
 
@@ -55,5 +88,7 @@ class MerkleTree:
     def get(self, data):'''
 
 if __name__ == '__main__':
-    L = [0, 1, 2, 3, 4]
+    L = [0, 1]
     m = MerkleTree(L)
+    m.insert(2)
+    m.insert(3)
